29c19713e4d3fd065d37599a57a19091e046edf1
MkDevOps <66654978+mukesh15dec@users.noreply.github.com>
2023-09-21 20:51:30 -0400

    Update JSON.py

--- Python/module/JSON.py
+++ Python/module/JSON.py
@@ -30,7 +30,7 @@ x = {
 }
 
 # convert into JSON:
-y = json.dumps(x)
+y = json.dumps(x)      # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  json.dumps
 
 # the result is a JSON string:
 print(y)
@@ -50,4 +50,39 @@ Example :
     }
 # convert into JSON:
 data=json.dumps(request_parameter, indent=2, default=str)
+
+# Format the Result
+# The example above prints a JSON string, but it is not very easy to read, with no indentations and line breaks.
+# The json.dumps() method has parameters to make it easier to read the result:
+
+json.dumps(x, indent=4)
+
+# Example :
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+# use four indents to make it easier to read the result:
+print(json.dumps(x, indent=4))
+
+# You can also define the separators, default value is (", ", ": "), which means using a comma and a space to separate each object, 
+# and a colon and a space to separate keys from values:
+
+json.dumps(x, indent=4, separators=(". ", " = "))
+
+
+
+
+
 
