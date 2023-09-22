e2e41c104105e9ea43b52a40324cfa37747a7e69
MkDevOps <66654978+mukesh15dec@users.noreply.github.com>
2023-09-21 20:27:39 -0400

    Update JSON.py

--- Python/module/JSON.py
+++ Python/module/JSON.py
@@ -1,9 +1,17 @@
 # JSON in Python
 import json
-#### some JSON:
+# some JSON:
 x =  '{ "name":"John", "age":30, "city":"New York"}'
-#### parse x:
+# parse x:
 y = json.loads(x)
-#### the result is a Python dictionary:
+# the result is a Python dictionary:
 print(y["age"])
+
+#example 2
+get_secret_value_response = client.get_secret_value(SecretId='snowMK-pwd')
+# parse get_secret_value_response :
+secret = json.loads(get_secret_value_response['SecretString'])
+# the result is a Python dictionary:
+pwd = secret['snowMK-pwd']
+
 
