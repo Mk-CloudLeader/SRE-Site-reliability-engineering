eb21c6da1fa2562384a547eb5c3a447a5c76765c
MkDevOps <66654978+mukesh15dec@users.noreply.github.com>
2023-09-21 20:41:20 -0400

    Update JSON.py

--- Python/module/JSON.py
+++ Python/module/JSON.py
@@ -1,9 +1,10 @@
 # JSON in Python
+# Convert from JSON to Python: json.loads. # If you have a JSON string, you can parse it by using the json.loads() method.
 import json
 # some JSON:
 x =  '{ "name":"John", "age":30, "city":"New York"}'
 # parse x:
-y = json.loads(x)
+y = json.loads(x)        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  json.loads
 # the result is a Python dictionary:
 print(y["age"])
 
@@ -14,4 +15,23 @@ secret = json.loads(get_secret_value_response['SecretString'])
 # the result is a Python dictionary:
 pwd = secret['snowMK-pwd']
 
+
+
+# Convert from Python to JSON: json.dumps
+# If you have a Python object, you can convert it into a JSON string by using the json.dumps() method.
+
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
 
